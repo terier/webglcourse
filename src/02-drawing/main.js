a535b72c73f0c6872da0ffdab0d7d7ce4bd39124
Žiga Lesar <terier.kuza@gmail.com>
2018-09-04 11:53:21 +0200

    Add examples

---
+++ src/02-drawing/main.js
@@ -0,0 +1,51 @@
+//@@../../shared/js/common.js
+//@@../../shared/js/WebGLUtils.js
+
+function load() {
+    canvas = document.querySelector('canvas');
+    gl = canvas.getContext('webgl');
+
+    var programs = WebGLUtils.compileShaders(gl, {
+        test: {
+            vertex: vertexShaderSource,
+            fragment: fragmentShaderSource
+        }
+    });
+    gl.useProgram(programs.test.program);
+
+    var data = new Float32Array([
+        -0.5, -0.5, 0.0, 1.0,
+        0.5, -0.5, 0.0, 1.0,
+        0.0, 0.5, 0.0, 1.0
+    ]);
+
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+
+    var aPosition = programs.test.attributes.aPosition;
+    gl.enableVertexAttribArray(aPosition);
+    gl.vertexAttribPointer(aPosition, 4, gl.FLOAT, false, 0, 0);
+}
+
+function animate() {
+    if (gl) {
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+    }
+}
+
+var vertexShaderSource = `
+attribute vec4 aPosition;
+
+void main() {
+    gl_Position = aPosition;
+}
+`.trim();
+
+var fragmentShaderSource = `
+precision mediump float;
+
+void main() {
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+`.trim();
---
+++ src/03-shader-vars/main.js
@@ -0,0 +1,70 @@
+//@@../../shared/js/common.js
+//@@../../shared/js/WebGLUtils.js
+
+var uTime;
+var timeStart;
+
+function load() {
+    canvas = document.querySelector('canvas');
+    gl = canvas.getContext('webgl');
+
+    var programs = WebGLUtils.compileShaders(gl, {
+        test: {
+            vertex: vertexShaderSource,
+            fragment: fragmentShaderSource
+        }
+    });
+    gl.useProgram(programs.test.program);
+
+    var data = new Float32Array([
+        -0.5, -0.5, 0.0, 1.0,  1.0, 0.0, 0.0, 1.0,
+        0.5, -0.5, 0.0, 1.0,   0.0, 1.0, 0.0, 1.0,
+        0.0, 0.5, 0.0, 1.0,    0.0, 0.0, 1.0, 1.0
+    ]);
+
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+
+    var aPosition = programs.test.attributes.aPosition;
+    gl.enableVertexAttribArray(aPosition);
+    gl.vertexAttribPointer(aPosition, 4, gl.FLOAT, false, 32, 0);
+
+    var aColor = programs.test.attributes.aColor;
+    gl.enableVertexAttribArray(aColor);
+    gl.vertexAttribPointer(aColor, 4, gl.FLOAT, false, 32, 16);
+
+    uTime = programs.test.uniforms.uTime;
+    timeStart = Date.now();
+}
+
+function animate() {
+    if (gl) {
+        gl.uniform1f(uTime, (Date.now() - timeStart) * 0.005);
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+    }
+}
+
+var vertexShaderSource = `
+attribute vec4 aPosition;
+attribute vec4 aColor;
+
+varying vec4 vColor;
+
+uniform float uTime;
+
+void main() {
+    gl_Position = aPosition + vec4(cos(uTime), sin(uTime), 0.0, 0.0) * 0.2;
+    vColor = aColor;
+}
+`.trim();
+
+var fragmentShaderSource = `
+precision mediump float;
+
+varying vec4 vColor;
+
+void main() {
+    gl_FragColor = vColor;
+}
+`.trim();
